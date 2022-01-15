2a9a2d5905046f803cb53cdef0cb5b9d0ebf93bb
Nick Foden <nickfoden@gmail.com>
2022-01-15 00:17:57 -0500

    coming soon setup initial

---
+++ app/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Form } from "remix";
+
+export default function Index() {
+  return (
+    <div style={{ 
+      display:"flex",
+      fontFamily: "courier, sans-serif", 
+      lineHeight: "1.4", 
+      width: "100vw" 
+      }}>
+      <h1 style={{margin: "4rem auto"}}>Coming Soon</h1>
+    </div>
+  );
+}
