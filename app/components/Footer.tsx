158a68c8a602fa223e2c3c5fb6ebc2d180706d7e
Nick Foden <nickfoden@gmail.com>
2022-04-06 22:31:17 -0400

    add image to main page (#4)

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => (
   <footer className="footer">
     <div className="column">
       <div className="footer_row">
-        <p>253 Grand Street</p>
+        <p>253 Grand Street &nbsp;</p>
         <p>Brooklyn, NY 11211</p>
       </div>
       <span className="footer_row">
         <a href="mailto:info@chinograndenyc.com">info@chinograndenyc.com</a>
         <span className="footer_space">&nbsp;-&nbsp;</span>
-        <a href="tel:19179090466">(917) 909-0466</a>
+        <a href="tel:19179090466">917 909-0466</a>
       </span>
     </div>
   </footer>
