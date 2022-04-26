35aab9d066aabe295481215732f344c5e1223bc0
Nick Foden <nickfoden@gmail.com>
2022-04-25 22:31:15 -0400

    Css breakpoints (#11)
    * css breakpoints in preparation
    
    * comment out unused css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,11 @@ import { Link } from "remix";
 const Header = () => (
   <nav className="header_nav">
     <Link to="/">
-      <img src={"chino-logo-straight.svg"} className="header_logo" />
+      <img
+        src={"chino-logo-straight.svg"}
+        className="header_logo"
+        alt="Chino Grande"
+      />
     </Link>
   </nav>
 );
